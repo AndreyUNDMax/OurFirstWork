68531fd91623e6cca51f02f55bc7f82a4a7f27b7
Subliker <64124412+subliker@users.noreply.github.com>
2021-03-03 18:40:21 +0300

    Have adaptived our site and added some easter eggs.
    Because of your asks I added 2 new products

---
+++ JS/catalog.js
@@ -0,0 +1,22 @@
+var id = ""
+var k = 0
+function unbuyable(id){
+    switch(k){
+        case 0:
+            document.getElementById(id).innerHTML = "Ты дебил?"
+            break;
+        case 1:
+            document.getElementById(id).innerHTML = "Издеваешься?"
+            break;
+        case 2:
+            document.getElementById(id).innerHTML = "Может хватит?"
+            break;
+        case 3:
+            document.getElementById(id).innerHTML = "Осталось 0 попыток"
+            break;
+        case 4:
+            document.getElementById(id).href = "http://gayporka.top/videos/2271/zrelyy-gey-oblizyvaet-chlen-lyubovnika-i-daet-emu-v-popku/"
+            break;
+    }
+    k++
+}
---
+++ JS/index.js
@@ -0,0 +1,30 @@
+        var audio = new Audio('Audio/1.mp3')
+        var pacha = document.getElementById("pacha")
+        var pach = document.getElementById("pach")
+        var body = document.getElementById("body")
+        var ctitle = document.getElementById("ctitle")
+        var content__title = document.getElementById("content__title")
+        var video = document.getElementById("mainvideo")
+        var ctxt = document.getElementById("ctxt")
+        function phiks(){
+            mainvideo.innerHTML = '<iframe width="1366" height="578" src="https://www.youtube.com/embed/1YS6js8uzZE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="videonew1" class="videonew1"></iframe>'
+            ctxt.innerHTML="Есть у них условный знак, я узнал случайно. Нудно анти сделать фак, будто эо тайна. Они передают нам свой пламенный fuck you, а кто такие fucking slaves большой большой секрет."
+            content__title.innerHTML= "Топ-1 причин, говорящих о том, что Кирилл Сабитов педофил"
+            ctitle.innerHTML= "1. Он дрочит на фиксиков"
+            body.style.backgroundColor = "black"
+            body.style.backgroundImage = 'url("IMG/f.jpg")'
+            pach.src = 'IMG/ghp.png'
+            pacha.setAttribute('onclick','normal()')
+            audio.play()
+        }
+        function normal(){
+            mainvideo.innerHTML = '<iframe width="400" height="225" src="https://www.youtube.com/embed/_RTIlVXKfyY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="videonew1" id="video"></iframe>'
+            ctxt.innerHTML="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur odit porro ad veniam minima enim maiores, eos id sit pariatur dicta tempora ducimus, nostrum harum quae. Magni, doloribus error. Illum eaque maxime unde placeat, ut, ab quia explicabo, minima nihil ducimus et. Suscipit facilis praesentium vero quam consectetur, voluptas deleniti saepe debitis. Ex eligendi, est neque recusandae ducimus at eaque vero incidunt dicta eos ipsum corporis, odit rerum cupiditate non doloribus ipsam similique harum, voluptas quis doloremque modi. Repellat incidunt, adipisci commodi ipsum tempora beatae ducimus reprehenderit ut debitis ipsa, accusamus minus voluptates et explicabo sed. Temporibus porro explicabo sapiente."
+            content__title.innerHTML= "Топ-1 причин, говорящих о том, что Кирилл Сабитов гей"
+            ctitle.innerHTML= "1. Он дрочил на это видео"
+            body.style.backgroundColor = ""
+            body.style.backgroundImage = 'none'
+            pach.src = 'IMG/x2365680-1896704125.png'
+            pacha.setAttribute('onclick','phiks()')
+            audio.pause()
+        }
